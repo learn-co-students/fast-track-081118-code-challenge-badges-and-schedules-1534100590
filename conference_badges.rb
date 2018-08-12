9f81aeacd3ec58e8ba9afa40d8cc7ecec12716f7
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-12 15:22:21 -0400

    Done -- the speaker array and method calls are not within the printer method, but can be. Instead, I made the printer method generic and it will print the contents of any array.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map do |name|
+    badge_maker name
+  end
+end
+
+def assign_rooms(speakers)
+  assignments = []
+  speakers.each_with_index do |speaker, index|
+    assignments << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+  end
+  assignments
+end
+
+def printer(array)
+  array.each do |element|
+    puts element
+  end
+end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+batched_badges = batch_badge_creator speakers
+printer batched_badges
+assignments = assign_rooms speakers
+printer assignments
