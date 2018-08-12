3afaa40d6e33c35fc7495b2ffd3257609044df75
Sebastian  Darena <sdseb2582@gmail.com>
2018-08-12 20:25:32 +0000

    finished

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+  
+def badge_badge_creator(speaker).each do |speaker|
+    speaker.each { "Dr. #{speaker} is an expert in ruby rails." }
+  
+def assign_rooms(suite).index do |suite|
+  suite.index { "#{speaker} is staying in #{suite.index += 1}" }
+  
+def printer(details)
+  puts "#{speaker} assigned to room #{suite.index}"
+  
+  end
+end
+  
+  speaker_array =["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
