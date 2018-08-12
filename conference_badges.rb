1d353ad92110729ad62338794c310e9d8c554853
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-12 19:35:10 +0000

    finish code challenge

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,43 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(names)
+  names.map {|name| badge_maker(name) }
+end
+
+
+def assign_rooms(names)
+  room_assignments = []
+  
+  names.each_with_index do |name, index|
+    room_number = index + 1
+    message = "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    room_assignments.push(message)
+  end
+  
+  return room_assignments
+end
+
+
+def printer(names)
+  badges = batch_badge_creator(names)
+  badges.each {|badge| puts badge}
+  
+  room_assignments = assign_rooms(names)
+  room_assignments.each {|message| puts message}
+  
+end
+
+
+
+# TEST CASES
+# speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+# puts badge_maker("Arel")
+# p batch_badge_creator(speakers)
+# p assign_rooms(speakers)
+# printer(speakers)
+
+
+
