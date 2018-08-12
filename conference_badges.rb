870465399e8b9b0330272ed98001b3465ef87146
kelvin Nwabuoku <kelvin_philips25@yahoo.com>
2018-08-12 20:19:11 +0000

    finished

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
+def going_swimming
+ puts going_swimming("mon", "tue", "wed", ).
+ 
+ def badge_maker
+   numbers= rooms
+   badge_maker ("1","2", "3","4", "5", "6", 7")
+   puts (badge_maker "1")
+   
+   
+   def list_of_speakers
+     list_of_speakers ("edsger", "ada", "charles", "alan", "grace", "linus", "matz"  )
+     
+     
+     list_of_speakers.each do |speakers|
+     puts "#{speakers} is badge_maker
+     
+     def assign_rooms
+      puts assign_rooms = list_of_speakers
+      
+       
+     
+   
+   
+   
+   
+   
+   
+   
+ 
+ 
+ 
+ 
+ 
+ end
+
+
 # Write your code here.
